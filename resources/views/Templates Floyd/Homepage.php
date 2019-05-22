f3550507ae6999cbd924c03abe37a95b357b22cd
Gismogy <31318693+Gismogy@users.noreply.github.com>
2019-05-22 14:19:50 +0200

    started working on views

---
+++ resources/views/Templates Floyd/Homepage.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="section">
+            <div class="row mb-5 justify-content-center">
+
+                <div class="col-md-7 text-center bg-secondary ">
+                    Collum one
+                    <br><br><br><br><br><br><br>
+                </div>
+
+            </div>   <!-- end row  -->
+
+
+            <div class="row mb-5 justify-content-center">
+                <div class="col-md-5 text-center bg-secondary mr-5">
+                    Left Side Col Row 2
+                    <br><br><br><br><br><br><br>
+                </div>
+
+                <div class="col-md-5 text-center bg-secondary ml-5">
+                    Right Side Col Row 2
+                    <br><br><br><br><br><br><br>
+                </div>
+
+            </div> <!-- row end -->
+
+            <div class="row mb-5 justify-content-center">
+
+                <div class="col-md-5 text-center bg-secondary mr-5">
+                    Left Side Col Row 2
+                    <br><br><br><br><br><br><br>
+                </div>
+
+                <div class="col-md-5 text-center bg-secondary ml-5">
+                    Right Side Col Row 2
+                    <br><br><br><br><br><br><br>
+                </div>
+
+
+            </div> <!-- row end -->
+        </div>  <!-- end section -->
+    </div> <!-- end container -->
+
+@endsection
