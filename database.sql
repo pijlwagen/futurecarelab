e599a44a84b238daae1f429ccdeaa5aa4e6d7a81
Michel3951 <tfhdgnetwork1@gmail.com>
2019-06-19 09:12:07 +0200

    too much to note

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2019 at 01:12 PM
+-- Generation Time: Jun 19, 2019 at 09:11 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -35,14 +35,6 @@ CREATE TABLE `answers` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `answers`
---
-
-INSERT INTO `answers` (`id`, `content`, `posted_by`, `created_at`, `updated_at`) VALUES
-(5, 'hoi', 1, '2019-06-14 07:06:12', '2019-06-14 07:06:12'),
-(6, 'klopt', 2, '2019-06-14 07:06:54', '2019-06-14 07:06:54');
 
 -- --------------------------------------------------------
 
@@ -121,6 +113,21 @@ CREATE TABLE `question_categories` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `question_categories`
+--
+
+INSERT INTO `question_categories` (`id`, `question_id`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, '2019-06-17 07:48:12', '2019-06-17 07:48:12'),
+(2, 2, 1, '2019-06-17 07:53:33', '2019-06-17 07:53:33'),
+(3, 3, 2, '2019-06-19 04:54:43', '2019-06-19 04:54:43'),
+(4, 4, 1, '2019-06-19 05:00:12', '2019-06-19 05:00:12'),
+(5, 5, 1, '2019-06-19 05:03:01', '2019-06-19 05:03:01'),
+(6, 6, 1, '2019-06-19 05:03:29', '2019-06-19 05:03:29'),
+(7, 7, 1, '2019-06-19 05:04:02', '2019-06-19 05:04:02'),
+(8, 8, 1, '2019-06-19 05:04:51', '2019-06-19 05:04:51'),
+(9, 9, 1, '2019-06-19 05:07:37', '2019-06-19 05:07:37');
+
 -- --------------------------------------------------------
 
 --
@@ -134,6 +141,21 @@ CREATE TABLE `question_tags` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `tag_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `question_tags`
+--
+
+INSERT INTO `question_tags` (`id`, `question_id`, `created_at`, `updated_at`, `tag_id`) VALUES
+(1, 1, '2019-06-17 07:48:12', '2019-06-17 07:54:29', 2),
+(2, 2, '2019-06-17 07:53:33', '2019-06-17 07:54:17', 3),
+(3, 3, '2019-06-19 04:54:43', '2019-06-19 04:54:43', 1),
+(4, 4, '2019-06-19 05:00:12', '2019-06-19 05:00:12', 1),
+(5, 5, '2019-06-19 05:03:01', '2019-06-19 05:03:01', 1),
+(6, 6, '2019-06-19 05:03:29', '2019-06-19 05:03:29', 1),
+(7, 7, '2019-06-19 05:04:02', '2019-06-19 05:04:02', 1),
+(8, 8, '2019-06-19 05:04:51', '2019-06-19 05:04:51', 1),
+(9, 9, '2019-06-19 05:07:37', '2019-06-19 05:07:37', 1);
 
 -- --------------------------------------------------------
 
@@ -194,16 +216,17 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `role` bigint(11) DEFAULT '1'
+  `role` bigint(11) DEFAULT '1',
+  `verified` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
-(1, 'Michel Bunschoten', 'noreply@michel3951.com', NULL, '$2y$10$CAUXi/Io8JZ0HM4P85jtPOA3DtE6AETqnsXm9l4LGtcLOQR5Viuxe', NULL, '2019-05-23 09:42:47', '2019-05-23 09:42:47', 2),
-(2, 'Vrijwilliger', 'bullshit@michel3951.com', NULL, '$2y$10$X09okqVwGMh.NcOJ3WitxeArygiTZTblaCDuFU4EvAeMkw9RoH7wm', NULL, '2019-06-14 07:06:40', '2019-06-14 07:06:40', 1);
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`, `verified`) VALUES
+(1, 'Michel Bunschoten', 'noreply@michel3951.com', NULL, '$2y$10$CAUXi/Io8JZ0HM4P85jtPOA3DtE6AETqnsXm9l4LGtcLOQR5Viuxe', NULL, '2019-05-23 09:42:47', '2019-05-23 09:42:47', 2, 0),
+(2, 'Vrijwilliger', 'bullshit@michel3951.com', NULL, '$2y$10$X09okqVwGMh.NcOJ3WitxeArygiTZTblaCDuFU4EvAeMkw9RoH7wm', NULL, '2019-06-14 07:06:40', '2019-06-14 07:06:40', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -282,7 +305,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -312,13 +335,13 @@ ALTER TABLE `question_answers`
 -- AUTO_INCREMENT for table `question_categories`
 --
 ALTER TABLE `question_categories`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `question_tags`
 --
 ALTER TABLE `question_tags`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `roles`
